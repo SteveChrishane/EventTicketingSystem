40682cfbf05765af26a30e485f2b9793669945d7
Steve Chrishane <chrishane.20230307@iit.ac.lk>
2024-11-07 16:42:09 +0530

    Added parameters to the Configuration class

--- src/main/java/com/company/Configuration.java
+++ src/main/java/com/company/Configuration.java
@@ -1,5 +1,11 @@
 package com.company;
 
 public class Configuration {
+    private int totalTickets;
+    private int ticketReleaseRate;
+    private int customerRetrievalRate;
+    private int maxTicketCapacity;
+
+
 
 }
--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -2,10 +2,6 @@ package com.company;
 
 public class Main {
 
-    private int totalTickets;
-    private int ticketReleaseRate;
-    private int customerRetrievalRate;
-    private int maxTicketCapacity;
 
 
 
