1d290ba6b516f107ef147b1f5e6c4c9d7d5461bf
Steve Chrishane <chrishane.20230307@iit.ac.lk>
2024-11-07 16:55:34 +0530

    Updated Vendor.java

--- src/main/java/com/company/Vendor.java
+++ src/main/java/com/company/Vendor.java
@@ -1,4 +1,22 @@
 package com.company;
 
-public class Vendor {
+public class Vendor implements Runnable {
+    private TicketPool ticketPool;
+    private int releaseRate;
+
+    public Vendor(TicketPool ticketPool, int releaseRate) {
+        this.ticketPool = ticketPool;
+        this.releaseRate = releaseRate;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(releaseRate);
+            ticketPool.addTickets(1);
+
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
 }
