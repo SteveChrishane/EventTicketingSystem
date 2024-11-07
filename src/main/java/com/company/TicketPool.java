430efc3240d027e055cf96620e173deec123ed4d
Steve Chrishane <chrishane.20230307@iit.ac.lk>
2024-11-07 16:54:31 +0530

    updated Ticketpool.java

--- src/main/java/com/company/TicketPool.java
+++ src/main/java/com/company/TicketPool.java
@@ -1,4 +1,23 @@
 package com.company;
 
 public class TicketPool {
+        
+    private int availableTickets;
+
+    public void addTickets(int count) {
+        availableTickets += count;
+        System.out.println(count + " tickets added.");
+        System.out.println("Total available tickets: " + availableTickets);
+
+    }
+
+    public void removeTickets(){
+        if (availableTickets > 0) {
+            availableTickets--;
+            System.out.println("Ticket purchased.");
+            System.out.println("Total available tickets: " + availableTickets);
+        } else {
+            System.out.println("No ticket purchased.");
+        }
+    }
 }
