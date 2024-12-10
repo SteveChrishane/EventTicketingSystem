a423eac2a18105cbef5449e8aa2cd6f3c3c9515d
Steve Chrishane <chrishane.20230307@iit.ac.lk>
2024-12-10 09:43:27 +0530

    Create Config.java

---
+++ backend/src/main/java/com/example/backend/config/Config.java
@@ -0,0 +1,27 @@
+package com.example.backend.config;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Config {
+    private int totalTickets;
+    private int ticketReleaseRate;
+    private int customerRetrievalRate;
+    private int maxTicketCapacity;
+
+    public int getTotalTickets() {
+        return totalTickets;
+    }
+
+    public int getTicketReleaseRate() {
+        return ticketReleaseRate;
+    }
+
+    public int getCustomerRetrievalRate() {
+        return customerRetrievalRate;
+    }
+
+    public int getMaxTicketCapacity() {
+        return maxTicketCapacity;
+    }
+}
